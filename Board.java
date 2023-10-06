71a5ad4a70ed690831264e7da48f30dc8ff6de43
Will Borgerding <146600482+BORGE369@users.noreply.github.com>
2023-10-05 19:51:53 -0500

    Add files via upload
    Project 2 Files. Inspiration and some code from the University of Minnesota.

---
+++ Board.java
@@ -0,0 +1,118 @@
+public class Board {
+
+    // Instance variables
+    private Piece[][] board;
+
+    //TODO:
+    // Construct an object of type Board using given arguments.
+    public Board() {
+
+    }
+
+    // Accessor Methods
+
+    //TODO:
+    // Return the Piece object stored at a given row and column
+    public Piece getPiece(int row, int col) {
+        return null;
+    }
+
+    //TODO:
+    // Update a single cell of the board to the new piece.
+    public void setPiece(int row, int col, Piece piece) {
+
+    }
+
+    // Game functionality methods
+
+    //TODO:
+    // Moves a Piece object from one cell in the board to another, provided that
+    // this movement is legal. Returns a boolean to signify success or failure.
+    // This method calls all necessary helper functions to determine if a move
+    // is legal, and to execute the move if it is. Your Game class should not 
+    // directly call any other method of this class.
+    // Hint: this method should call isMoveLegal() on the starting piece. 
+    public boolean movePiece(int startRow, int startCol, int endRow, int endCol) {
+        return false;
+    }
+
+    //TODO:
+    // Determines whether the game has been ended, i.e., if one player's King
+    // has been captured.
+    public boolean isGameOver() {
+        return false;
+    }
+
+    // Constructs a String that represents the Board object's 2D array.
+    // Returns the fully constructed String.
+    public String toString() {
+        StringBuilder out = new StringBuilder();
+        out.append(" ");
+        for(int i = 0; i < 8; i++){
+            out.append(" ");
+            out.append(i);
+        }
+        out.append('\n');
+        for(int i = 0; i < board.length; i++) {
+            out.append(i);
+            out.append("|");
+            for(int j = 0; j < board[0].length; j++) {
+                out.append(board[i][j] == null ? "\u2001|" : board[i][j] + "|");
+            }
+            out.append("\n");
+        }
+        return out.toString();
+    }
+
+    //TODO:
+    // Sets every cell of the array to null. For debugging and grading purposes.
+    public void clear() {
+
+    }
+
+    // Movement helper functions
+
+    //TODO:
+    // Ensure that the player's chosen move is even remotely legal.
+    // Returns a boolean to signify whether:
+    // - 'start' and 'end' fall within the array's bounds.
+    // - 'start' contains a Piece object, i.e., not null.
+    // - Player's color and color of 'start' Piece match.
+    // - 'end' contains either no Piece or a Piece of the opposite color.
+    public boolean verifySourceAndDestination(int startRow, int startCol, int endRow, int endCol, boolean isBlack) {
+        return false;
+    }
+
+    //TODO:
+    // Check whether the 'start' position and 'end' position are adjacent to each other
+    public boolean verifyAdjacent(int startRow, int startCol, int endRow, int endCol) {
+        return false;
+    }
+
+    //TODO:
+    // Checks whether a given 'start' and 'end' position are a valid horizontal move.
+    // Returns a boolean to signify whether:
+    // - The entire move takes place on one row.
+    // - All spaces directly between 'start' and 'end' are empty, i.e., null.
+    public boolean verifyHorizontal(int startRow, int startCol, int endRow, int endCol) {
+        return false;
+    }
+
+    //TODO:
+    // Checks whether a given 'start' and 'end' position are a valid vertical move.
+    // Returns a boolean to signify whether:
+    // - The entire move takes place on one column.
+    // - All spaces directly between 'start' and 'end' are empty, i.e., null.
+    public boolean verifyVertical(int startRow, int startCol, int endRow, int endCol) {
+        return false;
+    }
+
+    //TODO:
+    // Checks whether a given 'start' and 'end' position are a valid diagonal move.
+    // Returns a boolean to signify whether:
+    // - The path from 'start' to 'end' is diagonal... change in row and col.
+    // - All spaces directly between 'start' and 'end' are empty, i.e., null.
+    public boolean verifyDiagonal(int startRow, int startCol, int endRow, int endCol) {
+        return false;
+    }
+}
